51017dfe1a7c0237cd1093adc1d7ae026b4dc91b
Virtuoel <Virtuoel@users.noreply.github.com>
2018-10-02 13:08:32 +0300

    Active property

--- src/main/java/virtuoel/discarnate/block/BlockSpiritChanneler.java
+++ src/main/java/virtuoel/discarnate/block/BlockSpiritChanneler.java
@@ -3,6 +3,8 @@ package virtuoel.discarnate.block;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.MapColor;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.properties.PropertyBool;
+import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.BlockRenderLayer;
@@ -13,14 +15,35 @@ import virtuoel.discarnate.tileentity.TileEntitySpiritChanneler;
 
 public class BlockSpiritChanneler extends Block
 {
+	public static final PropertyBool ACTIVE = PropertyBool.create("active");
+	
 	public BlockSpiritChanneler(Material blockMaterialIn, MapColor blockMapColorIn)
 	{
 		super(blockMaterialIn, blockMapColorIn);
+		setDefaultState(getBlockState().getBaseState().withProperty(ACTIVE, false));
 	}
 	
 	public BlockSpiritChanneler(Material materialIn)
 	{
-		super(materialIn);
+		this(materialIn, materialIn.getMaterialMapColor());
+	}
+	
+	@Override
+	protected BlockStateContainer createBlockState()
+	{
+		return new BlockStateContainer(this, ACTIVE);
+	}
+	
+	@Override
+	public IBlockState getStateFromMeta(int meta)
+	{
+		return getDefaultState().withProperty(ACTIVE, meta == 1);
+	}
+	
+	@Override
+	public int getMetaFromState(IBlockState state)
+	{
+		return state.getValue(ACTIVE) ? 1 : 0;
 	}
 	
 	@Override
--- src/main/java/virtuoel/discarnate/tileentity/TileEntitySpiritChanneler.java
+++ src/main/java/virtuoel/discarnate/tileentity/TileEntitySpiritChanneler.java
@@ -1,8 +1,22 @@
 package virtuoel.discarnate.tileentity;
 
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 
 public class TileEntitySpiritChanneler extends TileEntity
 {
 	
+	public void activate(EntityPlayer player)
+	{
+		
+	}
+	
+	@Override
+	public boolean shouldRefresh(World world, BlockPos pos, IBlockState oldState, IBlockState newState)
+	{
+		return oldState.getBlock() != newState.getBlock();
+	}
 }
