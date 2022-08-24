8034986d012ad30bb950a8e0d38754d82dac4214
Virtuoel <Virtuoel@users.noreply.github.com>
2022-08-24 06:37:47 +0300

    Gamerule I18N and descriptions

--- src/main/java/virtuoel/discarnate/init/GameRuleRegistrar.java
+++ src/main/java/virtuoel/discarnate/init/GameRuleRegistrar.java
@@ -7,8 +7,8 @@ import virtuoel.discarnate.Discarnate;
 
 public class GameRuleRegistrar
 {
-	public static final GameRules.Key<GameRules.BooleanRule> PUMPKIN_TO_START = GameRuleRegistry.register(Discarnate.MOD_ID + ".pumpkinStart", GameRules.Category.MISC, GameRuleFactory.createBooleanRule(true));
-	public static final GameRules.Key<GameRules.BooleanRule> PUMPKIN_TO_CONTINUE = GameRuleRegistry.register(Discarnate.MOD_ID + ".pumpkinContinue", GameRules.Category.MISC, GameRuleFactory.createBooleanRule(false));
+	public static final GameRules.Key<GameRules.BooleanRule> PUMPKIN_TO_START = GameRuleRegistry.register(Discarnate.MOD_ID + ".pumpkinToStart", GameRules.Category.MISC, GameRuleFactory.createBooleanRule(true));
+	public static final GameRules.Key<GameRules.BooleanRule> PUMPKIN_TO_CONTINUE = GameRuleRegistry.register(Discarnate.MOD_ID + ".pumpkinToContinue", GameRules.Category.MISC, GameRuleFactory.createBooleanRule(false));
 	public static final GameRules.Key<GameRules.IntRule> MIN_LEVEL = GameRuleRegistry.register(Discarnate.MOD_ID + ".minLevel", GameRules.Category.MISC, GameRuleFactory.createIntRule(1, 0));
 	public static final GameRules.Key<GameRules.IntRule> LEVEL_COST = GameRuleRegistry.register(Discarnate.MOD_ID + ".levelCost", GameRules.Category.MISC, GameRuleFactory.createIntRule(1, 0));
 	
