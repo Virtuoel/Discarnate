a166a924d5a3cfbc158920009748387991131147
Virtuoel <Virtuoel@users.noreply.github.com>
2024-01-05 02:18:33 +0300

    Remove unused activation packet

--- src/main/java/virtuoel/discarnate/network/DiscarnatePacketHandler.java
+++ src/main/java/virtuoel/discarnate/network/DiscarnatePacketHandler.java
@@ -6,7 +6,7 @@ import virtuoel.discarnate.Discarnate;
 
 public class DiscarnatePacketHandler
 {
-	private static final String PROTOCOL_VERSION = Integer.toString(2);
+	private static final String PROTOCOL_VERSION = Integer.toString(3);
 	public static final SimpleChannel INSTANCE = NetworkRegistry.newSimpleChannel(
 		Discarnate.id("main"), () -> PROTOCOL_VERSION, PROTOCOL_VERSION::equals, PROTOCOL_VERSION::equals
 	);
@@ -14,7 +14,6 @@ public class DiscarnatePacketHandler
 	public static void init()
 	{
 		int regId = 0;
-		INSTANCE.registerMessage(regId++, ActivatePacket.class, ActivatePacket::encode, ActivatePacket::new, ActivatePacket::handle);
 		INSTANCE.registerMessage(regId++, TaskPacket.class, TaskPacket::encode, TaskPacket::new, TaskPacket::handle);
 	}
 }
